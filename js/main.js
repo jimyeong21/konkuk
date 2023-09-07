db57693816525ba24b659a1fdece721edb6e2e97
jimyeong <jimyeong21@gmail.com>
2023-09-07 21:13:25 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 let marginTit = $(".main-related .m-tit-wrap").offset().left;
 $(".main-program").css("padding-left",marginTit);
 
+$(".user-menu").on("mouseenter",function(){
+  $(this).addClass("open");
+  $(".user-depth01").addClass("on")
+})
+$(".user-menu").on("mouseleave",function(){
+  $(this).removeClass("open")
+})
