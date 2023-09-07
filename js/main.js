cf0862cd6c33e2b685f9b8345f54ded164d17ddd
jimyeong <jimyeong21@gmail.com>
2023-09-07 21:49:55 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -4,19 +4,14 @@ $(".main-program").css("padding-left",marginTit);
 
 $(".user-menu").on("mouseenter",function(){
   $(this).addClass("open");
-	setTimeout("menuOpen()", 100); 
+  $(".user-depth01").addClass("on");
 })
 $(".user-menu").on("mouseleave",function(){
   $(this).removeClass("open")
-  $(".user-depth01").fadeOut()
+  $(".user-depth01").removeClass("on");
 })
 
 $(".user-depth01 > li").on("click",function(){
   $(".user-depth01 > li").removeClass("on");
   $(this).addClass("on");
 })
-
-
-function menuOpen() {
-  $(".user-depth01").fadeIn(300)
-}
