7d2f2380d858083612196611d11949486d1f42f9
jimyeong <jimyeong21@gmail.com>
2023-09-07 21:27:20 +0900

    user메뉴 js 추가

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@ $(".main-program").css("padding-left",marginTit);
 
 $(".user-menu").on("mouseenter",function(){
   $(this).addClass("open");
-  $(".user-depth01").addClass("on")
+	setTimeout("menuOpen()", 100); 
 })
 $(".user-menu").on("mouseleave",function(){
   $(this).removeClass("open")
+  $(".user-depth01").fadeOut()
 })
+
+$(".user-depth01 > li").on("click",function(){
+  $(".user-depth01 > li").removeClass("on");
+  $(this).addClass("on");
+})
+
+
+function menuOpen() {
+  $(".user-depth01").fadeIn(300)
+}
